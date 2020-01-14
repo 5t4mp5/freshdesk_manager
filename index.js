ae096cf81a62036fafa3d9e6214946cda656d22c
Dave <dave.e.siegel@gmail.com>
2020-01-14 15:46:21 -0500

    append to last commit

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
 import selfAssignTickets from './selfAssignTickets.js';
 
-// const selfAssignTickets = () => {
-//   return searchTickets('status:2 OR status:3 OR status:6 OR status:7')
-//     .then(results => {
-//       const tickets = results.filter(ticket => ticket.responder_id === null);
-//       console.log(tickets.length);
-//       return Promise.all(
-//         tickets.map(ticket => {
-//           console.log('ID: ', ticket.id);
-//           return axios
-//             .put(
-//               `https://pricereporter.freshdesk.com/api/v2/tickets/${ticket.id}`,
-//               { responder_id: 16000091123 },
-//               {
-//                 headers: { Authorization: process.env.API_KEY },
-//               }
-//             )
-//             .catch(e => {
-//               throw e;
-//             });
-//         })
-//       );
-//     })
-//     .then(results => {
-//       results.forEach(result => {
-//         const { id, subject, description } = result.data;
-//         email(id, subject, description);
-//       });
-//     })
-//     .catch(e => {
-//       throw e;
-//     });
-// };
-
 setInterval(selfAssignTickets, 60000);
