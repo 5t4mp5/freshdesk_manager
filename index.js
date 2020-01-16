f1d4ab25f646c260133d3d7113bc6a6861f07c83
Dave <dave.e.siegel@gmail.com>
2020-01-16 12:22:00 -0500

    add procfile for heroku to use as worker

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ import schedule from 'node-schedule';
 
 //set up scheduler for morning reports
 const morningRule = new schedule.RecurrenceRule();
-morningRule.hour = 9;
+morningRule.hour = 12;
+morningRule.minute = 5;
 
 const openReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReports.aging('open')
+  generateReports.aging('open').catch(e => console.log(e))
 );
 
 const waitingCustReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReports.aging('waiting_customer')
+  generateReports.aging('waiting_customer').catch(e => console.log(e))
 );
 
 const waitingThirdReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReports.aging('waiting_third_party')
+  generateReports.aging('waiting_third_party').catch(e => console.log(e))
 );
 
 const pendingReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReports.aging('pending')
+  generateReports.aging('pending').catch(e => console.log(e))
 );
 
 //Check for open unassigned tickets regularly and assign
