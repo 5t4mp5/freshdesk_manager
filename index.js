bf6b6e61235838a44fca6a299b7dbab96ebf0233
Dave <dave.e.siegel@gmail.com>
2020-01-17 15:28:37 -0500

    refactored report scheduling to its own module.

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 import selfAssignTickets from './selfAssignTickets.js';
-import generateReport from './generateReport.js';
-import schedule from 'node-schedule';
-
-//set up scheduler for morning reports
-const morningRule = new schedule.RecurrenceRule();
-morningRule.hour = 8;
-morningRule.minute = 0;
-
-const openReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReport('open').catch(e => console.log(e))
-);
-
-const waitingCustReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReport('waiting_customer').catch(e => console.log(e))
-);
-
-const waitingThirdReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReport('waiting_third_party').catch(e => console.log(e))
-);
-
-const pendingReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReport('pending').catch(e => console.log(e))
-);
+import scheduleReports from './scheduleReports.js';
 
 //Check for open unassigned tickets regularly and assign
 setInterval(selfAssignTickets, 60000);
 
-generateReport('pending');
+//Initialize report schedule
+scheduleReports(['open', 'pending', 'waiting_customer', 'waiting_third_party'])
+  .then(() => console.log('REPORT SCHEDULE INITIALIZED'))
+  .catch(e => console.error(e));
