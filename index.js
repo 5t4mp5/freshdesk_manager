13eeee41c369aa58873a80bcc92c6f863cd3c9c9
Dave <dave.e.siegel@gmail.com>
2020-01-17 15:06:48 -0500

    Refactored generateReport. Renamed exports in a way that makes more sense.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import selfAssignTickets from './selfAssignTickets.js';
-import * as generateReports from './generateReports.js';
+import generateReport from './generateReport.js';
 import schedule from 'node-schedule';
 
 //set up scheduler for morning reports
@@ -8,20 +8,22 @@ morningRule.hour = 8;
 morningRule.minute = 0;
 
 const openReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReports.aging('open').catch(e => console.log(e))
+  generateReport('open').catch(e => console.log(e))
 );
 
 const waitingCustReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReports.aging('waiting_customer').catch(e => console.log(e))
+  generateReport('waiting_customer').catch(e => console.log(e))
 );
 
 const waitingThirdReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReports.aging('waiting_third_party').catch(e => console.log(e))
+  generateReport('waiting_third_party').catch(e => console.log(e))
 );
 
 const pendingReportJob = schedule.scheduleJob(morningRule, () =>
-  generateReports.aging('pending').catch(e => console.log(e))
+  generateReport('pending').catch(e => console.log(e))
 );
 
 //Check for open unassigned tickets regularly and assign
 setInterval(selfAssignTickets, 60000);
+
+generateReport('pending');
