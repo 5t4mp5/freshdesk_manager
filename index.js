71bcf9d8b665a42d97241ef3252d7c2382bc9c83
Dave <dave.e.siegel@gmail.com>
2020-01-20 12:20:46 -0500

    removed test code accidentally included with last commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import selfAssignTickets from './selfAssignTickets.js';
 import scheduleReports from './scheduleReports.js';
-import generateReport from './generateReport.js';
 
 //Check for open unassigned tickets regularly and assign
 setInterval(selfAssignTickets, 60000);
@@ -9,7 +8,3 @@ setInterval(selfAssignTickets, 60000);
 scheduleReports(['open', 'pending', 'waiting_customer', 'waiting_third_party'])
   .then(() => console.log('REPORT SCHEDULE INITIALIZED'))
   .catch(e => console.error(e));
-
-generateReport('open')
-  .then(() => console.log('REPORT DONE'))
-  .catch(e => console.log(e));
